dcb4df11c49b6d65656808dbae0dc860d3b67eac
leontin-vasii <leontin.vasii@gmail.com>
2022-05-04 22:33:29 +0300

    First

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,11 @@
+package ro.java.ctrln;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //JAR - Java Archive
+        //Java_Home
+        System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+
+    }
+}
